bfa92cf098ddd9e5cd4e711fb7ffd9e48e2a182a
oAngulo-sh <andrewsmoehlecke@gmail.com>
2022-02-06 11:39:46 -0300

    alterando path do call back de cadastro

--- avaliacao/php/exec_cadastrar.php
+++ avaliacao/php/exec_cadastrar.php
@@ -10,4 +10,4 @@ $sql = "insert into cadastro (nome, telefone) values ('$nome', '$tel')";
 mysqli_query($conexao, $sql) or die(mysqli_connect_error());
 
 $msg = urlencode('Participante cadastrado com sucesso!');
-//header("location: ../php/form_cadastrar.php?retorno=$msg");
+header("location: ../screens/cadastrar/form_cadastrar.php?retorno=$msg");
