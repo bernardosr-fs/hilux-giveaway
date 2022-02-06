27e2a15a0bbced48a5923d9921b4f0378fb2efd7
Your Name <bernardosr@hotmail.com.br>
2022-02-06 15:27:11 -0300

    só vai

--- avaliacao/screens/menu/menu.php
+++ avaliacao/screens/menu/menu.php
@@ -16,30 +16,48 @@
       <img src="../../assets/img/hilux.png" alt="camionete hilux cabine dupla">
     </div>
     <div class="inscritos">
-      <span>Inscritos</span>
-      <a href="">
-        <img src="../../assets/icons/lupa-icon.png" />
-      </a>
+      <div class="titulo">
+        <span>Inscritos</span>
+        <a href="../pesquisar/form_pesquisar.php">
+          <img class="icone" src="../../assets/icons/lupa-icon.png" />
+        </a>
+      </div>
       <div class="painel-inscritos">
-        <?php
-        require_once('../../php/conectar.php');
+        <table class="tabela">
+          <tr class="legenda-tabela">
+            <th>Nome </th>
+            <th>Telefone </th>
+            <th>Editar </th>
+            <th>Excluir </th>
+          </tr>
+          <?php
+          require_once('../../php/conectar.php');
 
-        $sql = "select * from cadastro";
-        $res = mysqli_query($conexao, $sql) or die(mysqli_connect_error());
+          $sql = "select * from cadastro";
+          $res = mysqli_query($conexao, $sql) or die(mysqli_connect_error());
 
-        if ($res->num_rows > 0) {
-          while ($linha = $res->fetch_assoc()) {
-            $url_editar =  '<a href="../atualizar/form_atualizar.php?cod=' . $linha["codigo"] . '"><img src="../../assets/icons/edit-icon.png"></a>';
-            $url_excluir = '<a href="../excluir/form_excluir.php?cod=' . $linha["codigo"] . '"><img src="../../assets/icons/delete-icon.png"></a>';
-            echo 'Nome: ' . $linha["nome"] . ' - Telefone: ' . $linha["telefone"] . $url_editar . $url_excluir;
+          if ($res->num_rows > 0) {
+            while ($linha = $res->fetch_assoc()) {
+              $url_editar =  '<a href="../atualizar/form_atualizar.php?cod=' . $linha["codigo"] . '"><img class = "icone" src="../../assets/icons/edit-icon.png"></a>';
+              $url_excluir = '<a href="../excluir/form_excluir.php?cod=' . $linha["codigo"] . '"><img class = "icone" src="../../assets/icons/delete-icon.png"></a>';
+              $dados = $linha["nome"] . '' . $linha["telefone"] . $url_editar . $url_excluir;
+              $nome = '<span>' . $linha["nome"] . '</span>';
+              $telefone = '<span>' . $linha["telefone"] . '</span>';
+
+              echo '<tr class = "dados-tabela"> <td>' . $nome . '</td>';
+              echo '<td>' . $telefone . '</td>';
+              echo '<td>' . $url_editar . '</td>';
+              echo '<td>' . $url_excluir . '</td> </tr>';
+            }
           }
-        }
-        ?>
+          ?>
       </div>
-      <div>
-        <a href="../cadastrar/form_cadastrar.php">Cadastre-se</a>
-      </div>
+      </table>
     </div>
+    <div>
+      <a href="../cadastrar/form_cadastrar.php">Cadastre-se</a>
+    </div>
+  </div>
 
   </div>
 
